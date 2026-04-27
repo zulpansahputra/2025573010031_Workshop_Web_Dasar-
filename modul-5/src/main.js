79e9f55ece97094ff0e1454a3dac5c54e9408a82
zulpansahputra <zulpansahputra0@gamil.com>
2026-04-27 20:42:30 +0700

    bagian-4-komponen UI & dark mode

--- modul-5/src/main.js
+++ modul-5/src/main.js
@@ -1,4 +1,18 @@
 // main.js
-import './style.css'
+import "./style.css";
 // Hapus semua kode lain di main.js (template default Vite)
 // Kita akan menulis HTML langsung di index.html
+// Dark mode toggle
+const btn = document.getElementById("toggleDark");
+const html = document.documentElement;
+// Cek preferensi tersimpan
+if (localStorage.getItem("theme") === "dark") {
+  html.classList.add("dark");
+  btn.textContent = "light";
+}
+btn.addEventListener("click", () => {
+  html.classList.toggle("dark");
+  const isDark = html.classList.contains("dark");
+  btn.textContent = isDark ? "light" : "dark";
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+});
